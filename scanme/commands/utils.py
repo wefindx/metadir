aead7286168c0dd58da54ee460f515567ffb81e0
Mindey <mindey@qq.com>
2018-11-04 16:15:19 +0000

    remove filters

--- scanme/commands/utils.py
+++ scanme/commands/utils.py
@@ -6,28 +6,14 @@ def nested_set(dic, keys, value):
     dic[keys[-1]] = value
 
 
-def dir_metatree(
-        _ROOT_='.',
-        ignore_dot_files=True,
-        ignore_extensions=['.pyc', '.tar.gz'],
-        ignore_dirs=['__pycache__', '.egg-info', 'dist']
-    ):
+def dir_metatree(_ROOT_='.'):
 
     _LEAF_ = {'*': ''}
     data = {}
 
     for dirName, subdirList, fileList in os.walk(_ROOT_):
 
-        # SKIP
-        if dirName.startswith('./.') and ignore_dot_files:
-            continue
-
         dirPath = dirName.split('/')[1:]
-
-        # SKIP
-        if dirPath:
-            if any([dirPath[-1].endswith(ignore) for ignore in ignore_dirs]):
-                continue
 
         tree = {}
         for k in reversed(dirPath):
@@ -36,10 +22,6 @@ def dir_metatree(
         data.update(tree)
 
         for fname in fileList:
-
-            # SKIP
-            if any([fname.endswith(ignore) for ignore in ignore_extensions]):
-                continue
 
             if dirPath:
                 nested_set(data, dirPath, {fname: _LEAF_})
